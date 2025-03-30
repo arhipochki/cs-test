c582348c0ccc2968aa7bf913985bc28f7abbd973
Alexey Arkhipov <lexa.nf@yandex.ru>
2025-03-30 21:53:23 +0300

    Structure changes

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-g++ .\main_oop.cpp -o main_oop.exe -s -DNDEBUG -O3 -std=c++23
-g++ .\main_std.cpp -o main_std.exe -s -DNDEBUG -O3 -std=c++23
+g++ .\src\main_oop.cpp -o main_oop.exe -s -DNDEBUG -O3 -std=c++23
+g++ .\src\main_std.cpp -o main_std.exe -s -DNDEBUG -O3 -std=c++23
